c233b5b89660e5c6eaf978e33c9e2d852c98f40a
Rtumen1d02 <>
2026-06-12 00:37:52 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -5,7 +5,7 @@ categories = "Candy Aisle, Pasta Aisle"
 #slice the items string to extract
 candy1 = items[0:9]
 candy2 = items[11:20]
-dry_goods = categories[13:18]
+dry_goods = items[22:27]
 #slice categories string to extract
 category1 = categories[0:11]
 category2 = categories[13:24]
