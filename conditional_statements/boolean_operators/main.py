bed2c95352a5e0beda71ed1d6f1582e957bdb7e1
Rtumen1d02 <>
2026-06-15 21:53:28 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,5 @@
+total_cost = 25.00
+discountEligible_threshold = 20.00
+
+discountEligible = total_cost >= discountEligible_threshold
+print("Is the purchase elegible for a discount", discountEligible)
