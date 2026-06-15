e794c8f1ada51af24eec37bbb54837dff8cbd0fd
Rtumen1d02 <>
2026-06-15 21:58:33 +0000

    Boolean Operators

--- conditional_statements/boolean_operators/main.py
+++ conditional_statements/boolean_operators/main.py
@@ -2,4 +2,4 @@ total_cost = 25.00
 discountEligible_threshold = 20.00
 
 discountEligible = total_cost >= discountEligible_threshold
-print("Is the purchase elegible for a discount", discountEligible)
+print("is the purchase eligible for a discount?", discountEligible)
