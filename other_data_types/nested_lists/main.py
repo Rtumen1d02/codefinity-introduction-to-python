63776bb63e77efaec5c4b600c71c72afe2b61dec
Rtumen1d02 <>
2026-06-23 00:05:47 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,15 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+#remove onions
+vegetables.remove("onions")
+#add carrots
+vegetables.append("carrots")
+#add cucumbers
+vegetables.append("cucumbers")
+#sort vegetables list
+vegetables.sort()
+#Print updated vegetables list
+print("Updated Vegetable Inventory:", vegetables)
+#print carrots are in the list
+print("Carrots are already in the list", vegetables[0])
+#print cucumbers are already in the list
+print("Cucumbers are already in the list", vegetables[1])
